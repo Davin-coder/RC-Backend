086a1896f3fa2b10d73d09b3e4ff6f734d935cdf
walex0627 <walteralex0627@gmail.com>
2025-08-31 20:55:51 -0500

    Create the endpoints of templates

--- app.js
+++ app.js
@@ -2,30 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import session from "express-session";
+
 import userRoutes from "./src/routes/users/userRoutes.js";
 import groupsRoutes from "./src/routes/groups/groupsRoutes.js";
 import eventsRoutes from "./src/routes/events/eventsRoutes.js";
 import eventRegistrationsRoutes from "./src/routes/events/registrationsRoutes.js";
 import hackathonsRoutes from "./src/routes/hackathons/hackathonsRoutes.js";
 import challengesRoutes from "./src/routes/challenges/challengesRoutes.js";
-import projectsRoutes from "./src/routes/projects/projectsRoutes.js"
-
+import projectsRoutes from "./src/routes/projects/projectsRoutes.js";
+import templateRoutes from "./src/routes/templates/templateRoutes.js";   // 👈 nueva importación
 
 dotenv.config();
 const app = express();
 const PORT = process.env.EXPRESSPORT || 3000;
 
-// CORS: permite al front (Vite) hacer fetch con cookies
+// CORS
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
 }));
 
-// Body parsers (una sola vez)
+// Body parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ============= // Login EndPoint // =============
+// Sesiones
 app.use(session({
   secret: process.env.SESSION_SECRET || "1234567890",
   resave: false,
@@ -39,11 +40,6 @@ app.use(session({
 
 // Healthcheck
 app.get("/", (_req, res) => res.send("✅ Server is running!"));
-
-// test endpoint
-app.get("/", (req, res) => {
-  res.send("✅ Server is running!");
-});
 
 // ============= // Users EndPoints // =============
 app.use("/users", userRoutes);
@@ -63,6 +59,9 @@ app.use("/challenges", challengesRoutes);
 
 // ============= // Projects EndPoints // =============
 app.use("/projects", projectsRoutes);
+
+// ============= // Templates EndPoints // =============
+app.use("/templates", templateRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
